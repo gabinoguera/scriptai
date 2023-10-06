7101b5dd0a9621dd96ab3708344b229724c1ee63
gabi.noguera <68296070+gabinoguera@users.noreply.github.com>
2023-10-06 11:39:55 +0200

    Update script_estructura_articulos_v2.py

--- script_estructura_articulos_v2.py
+++ script_estructura_articulos_v2.py
@@ -1,11 +1,5 @@
 import openai
-import pandas as pd
 import os
-
-# Leemos el archivo CSV que subimos al entorno de Colab(el encoding que me ha funcionado es Latin pero se puede probar con utf-8)
-
-#df = pd.read_csv('diplomados_edicion.csv', encoding='latin-1')
-#df = pd.read_csv('question.csv', encoding='utf-8')
 
 # Cargar las variables de entorno desde el archivo .env
 from dotenv import load_dotenv, find_dotenv
@@ -16,11 +10,6 @@ openai.api_key = os.getenv('API_GPT_KEY')
 
 #Keyword semilla
 keyword= "diplomatura marketing digital"
-
-#for i, row in df.iterrows():
-    #question = row["title"]
-    #messages.append({"role": "user", "content": question})
-
 
 prompt = f"""Crea la estructura de un artículo para un blog especializado en cursos de marketing digital. 
 El artículo debe seguir esta estructura:
@@ -58,11 +47,6 @@ except Exception as e:
 
 # Obtener el resultado del proceso
 result = response['choices'][0]['message']['content']
-
-# Crear un archivo de texto y guardar el resultado (.txt)
-#with open('resultante.txt', 'w', encoding='utf-8') as file:
- #   file.write(result.replace('\n', ''))
-#print("Proceso completado. El resultado se ha guardado en 'resultante.txt'.")
 
 # Almacena el resultado en una variable
 generated_prompt = result.replace('\n', '')
